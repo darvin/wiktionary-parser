7f2fe130b090591e20cbcc7874d9dfbd015c2f28
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-09-13 16:59:33 -0700

    initial parser and test

--- test/parserTests.js
+++ test/parserTests.js
@@ -5,13 +5,19 @@ var etymolator = require('../');
 var path = require('path');
 var fs = require('fs');
 
-describe('etymolator', function() {
+describe('etymolator parser', function() {
   describe('parser', function () {
-    it('should parse wiki', function () {
-    	var wikitext = fs.readFileSync(path.join(__dirname,"fixtures", "test.wiki"), {encoding:'utf8'});
+  	var r = null;
+  	before(function() {
+			var wikitext = fs.readFileSync(path.join(__dirname,"fixtures", "test.wiki"), {encoding:'utf8'});
 
-      var result = etymolator.parseWiki(wikitext);
-      expect(result).to.be.ok;
+      r = etymolator.parseWiki(wikitext);	  
     });
+
+    it('should parse wiki', function () {
+      expect(r).to.be.ok;
+    });
+
+    
   });
 });
