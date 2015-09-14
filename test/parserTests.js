989efcb0dc6592cb3ab53acaeb0f0d783188101a
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-09-13 17:53:19 -0700

    parser start

--- test/parserTests.js
+++ test/parserTests.js
@@ -6,7 +6,7 @@ var path = require('path');
 var fs = require('fs');
 
 describe('etymolator parser', function() {
-  describe('parser', function () {
+  describe('parses "test"', function () {
   	var r = null;
   	before(function() {
 			var wikitext = fs.readFileSync(path.join(__dirname,"fixtures", "test.wiki"), {encoding:'utf8'});
@@ -17,7 +17,51 @@ describe('etymolator parser', function() {
     it('should parse wiki', function () {
       expect(r).to.be.ok;
     });
+    it('should have multiple languages');
+    describe('parsed english word', function() {
+    	var w = null;
+    	before(function() {
+    		w = r["eng"];
+    	});
+    	it('should have 2 meanings');
+    	describe('meaning 1', function() {
+    		it('should have etymology');
+    		it('should have noun role');
+    		it('should have verb role');
+    	});
+    	describe('meaning 2', function() {
+    		it('should have etymology');
+    		it('should have noun role');
+    		it('should have verb role');
+    	});
 
+
+
+    })
+  });
+
+  describe('parses "sample"', function () {
+  	var r = null;
+  	before(function() {
+			var wikitext = fs.readFileSync(path.join(__dirname,"fixtures", "sample.wiki"), {encoding:'utf8'});
+
+      r = etymolator.parseWiki(wikitext);	  
+    });
+
+    it('should parse wiki', function () {
+      expect(r).to.be.ok;
+    });
+    describe('parsed english word', function() {
+    	var w = null;
+    	before(function() {
+    		w = r["eng"];
+    	});
+    	it('should have 1 meaning');
+    	describe('meaning 1', function() {
+    		it('should have noun role');
+    	});
     
+
+    })
   });
 });
